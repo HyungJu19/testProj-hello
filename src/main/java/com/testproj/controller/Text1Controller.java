0ded669b25e34af948f7fb5c2163979d9736526a
Lampra1208 <gkgk7610@naver.com>
2023-11-18 11:38:22 +0900

    feat: 이기원 수정(23.11.18).3

--- src/main/java/com/testproj/controller/Text1Controller.java
+++ src/main/java/com/testproj/controller/Text1Controller.java
@@ -2,6 +2,8 @@ package com.testproj.controller;
 
 public class Text1Controller {
     public static void main(String[] args) {
-        System.out.println("이기원 변동사항");
+        System.out.println("이기원 변동 사항");
+        System.out.println("이래도 안되나?");
+        
     }
 }
