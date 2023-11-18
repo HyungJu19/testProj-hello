25ce676163d26861328928beca99b5e74132c4e3
Lampra1208 <gkgk7610@naver.com>
2023-11-18 12:23:31 +0900

    Merge branch '기원' of C:\Users\82102\OneDrive\문서\GitHub\testProj-hello with conflicts.

--- src/main/java/com/testproj/controller/Text1Controller.java
+++ src/main/java/com/testproj/controller/Text1Controller.java
@@ -5,5 +5,9 @@ public class Text1Controller {
         System.out.println("이기원 변동 사항");
         System.out.println("이래도 안되나?");
 
+        System.out.println("작업 하던거는 날라가나?");
+        System.out.println("작업 하던거는 날라가나?2");
+        System.out.println("작업 하던거는 날라가나?3");
+
     }
 }
