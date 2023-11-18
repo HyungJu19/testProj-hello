ca3318c5f392af373a9087532b7a570a1afa7090
Lampra1208 <143512926+Lampra1208@users.noreply.github.com>
2023-11-18 13:10:29 +0900

    기원 (#7)
    * Merge branch '기원' of C:\Users\82102\OneDrive\문서\GitHub\testProj-hello with conflicts.
    
    * feat: 이기원 수정(23.11.18).4
    
    * feat: 이기원 수정(23.11.18).4
    
    ---------
    
    Co-authored-by: HyungJu19 <143996496+HyungJu19@users.noreply.github.com>

--- src/main/java/com/testproj/controller/Text1Controller.java
+++ src/main/java/com/testproj/controller/Text1Controller.java
@@ -5,11 +5,5 @@ public class Text1Controller {
         System.out.println("이기원 변동 사항");
         System.out.println("이래도 안되나?");
 
-        System.out.println("작업 하던거는 날라가나?");
-        System.out.println("작업 하던거는 날라가나?2");
-        System.out.println("작업 하던거는 날라가나?3");
-
-        System.out.println("작업 하던거는 날라가나?4");
-
     }
 }
