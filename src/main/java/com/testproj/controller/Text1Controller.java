026f1385c3976ccb67adcb043050fbed5c22d491
HyungJu19 <143996496+HyungJu19@users.noreply.github.com>
2023-11-18 13:32:44 +0900

    feat: TestController  내용추가

--- src/main/java/com/testproj/controller/Text1Controller.java
+++ src/main/java/com/testproj/controller/Text1Controller.java
@@ -11,5 +11,14 @@ public class Text1Controller {
         System.out.println("왜 안되는걸까요3");
 
         System.out.println("진짜 좀 되라");
+        System.out.println("이기원 변동 사항");
+        System.out.println("이래도 안되나?");
+        System.out.println("왜 안되는걸까요1");
+        System.out.println("왜 안되는걸까요2");
+
+
+        System.out.println("왜 안되는걸까요3");
+
+        System.out.println("진짜 좀 되라");
     }
 }
